5eb6c0fad81a45e2ea1bc253777095e1e35e31b1
kokkeanshern <kokkeanshern@gmail.com>
2025-07-25 16:27:18 +0800

    use env vars

--- fundaztic/main.py
+++ fundaztic/main.py
@@ -1,9 +1,11 @@
+import os
+import time
+import logging
 from selenium import webdriver
 from locators import login
 from constants import Captcha
 from captcha.ocr import solve_captcha
-import logging
-import time
+
 
 if __name__ == "__main__":
     # Setup logging
@@ -12,8 +14,11 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://p2p.fundaztic.com/visitor/to-login")
 
-    login.resolve_email_input(driver, "test@test.com")
-    login.resolve_password_input(driver, "test123")
+    email = os.environ["EMAIL"]
+    password = os.environ["PASSWORD"]
+
+    login.resolve_email_input(driver, email)
+    login.resolve_password_input(driver, password)
 
     # Saves the captcha image from the browser in the project directory.
     login.save_captcha_image(driver, Captcha.raw_image_file)
